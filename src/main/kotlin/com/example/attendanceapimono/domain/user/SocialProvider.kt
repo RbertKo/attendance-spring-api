93b778979d88d799d00dabbdbd91f4bb6d550a75
RbertKo <myeongsku@gmail.com>
2021-07-25 23:28:27 +0900

    Fix: Composite-id class must implement issue

--- src/main/kotlin/com/example/attendanceapimono/domain/user/SocialProvider.kt
+++ src/main/kotlin/com/example/attendanceapimono/domain/user/SocialProvider.kt
@@ -1,5 +1,6 @@
 package com.example.attendanceapimono.domain.user
 
+import java.io.Serializable
 import javax.persistence.*
 
 enum class SocialType {
@@ -8,17 +9,17 @@ enum class SocialType {
 
 @Entity
 @Table(name = "social_providers")
-class SocialProvider(
+class SocialProvider(id: String, type: SocialType, user: User): Serializable {
     @Id
     @Column(length = 30)
-    val id: String,
+    val id: String = id;
 
     @Id
     @Enumerated(EnumType.STRING)
     @Column(length = 10)
-    val type: SocialType,
+    val type: SocialType = type;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id") // user.id
-    val user: User?,
-)
+    val user: User = user;
+}
